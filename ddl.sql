a44d201cc11d0e072a74d6349f089c1b6faefadd
Ola Ipaye <ipayeola@outlook.com>
2025-09-26 22:54:10 +0100

    updated ddl.sql file with tcl(transaction control language), to start transaction, perform multiple operations, commit if no errors or rollback if there is.

--- ddl.sql
+++ ddl.sql
@@ -49,6 +49,35 @@ FLUSH PRIVILEGES; -- Apply changes to the user access
 -- revoke permission to the user
 REVOKE INSERT ON sales_data.* FROM 'local_user'@'localhost';
 
+-- TCL - Transaction Control Language
+-- start transaction
+START TRANSACTION;
+
+-- multiple operations treated as one
+UPDATE products SET stock_quantity = stock_quantity - 2 WHERE product_id = 2;
+INSERT INTO sales (sale_id, product_id, quantity, sale_date)
+VALUES (107, 2, 2, '2025-09-26');
+
+COMMIT;
+
+ROLLBACK; -- only if error in above operations
+
+
+-- poor data quality - failed transaction with rollback
+-- start transaction
+START TRANSACTION;
+
+-- reduce stock for a product that doesn't exist in the products table (id = 999)
+UPDATE products 
+SET stock_quantity = stock_quantity - 1
+WHERE product_id = 999;
+
+-- insert a sale for the product that doesn't exists
+INSERT INTO sales (sale_id, product_id, quantity, sale_date)
+VALUES (201, 999, 1, '2025-09-27');
+
+-- realize error, so cancel everything - no changes saved
+ROLLBACK;
 
 
 
