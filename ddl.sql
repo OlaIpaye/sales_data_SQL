65a7f9abc660f45a121477a438621f00f59afb45
Ola Ipaye <ipayeola@outlook.com>
2025-09-26 21:53:59 +0100

    updated ddl.sql file with dml statements to insert a record, update a column, delete record from a table.

--- ddl.sql
+++ ddl.sql
@@ -1,5 +1,6 @@
 USE sales_data;
 
+-- DDL - Data Definition Language
 -- create customer table
 CREATE TABLE customers (
 	customer_id INT PRIMARY KEY,
@@ -14,3 +15,32 @@ DROP TABLE customers;
 ALTER TABLE products
 ADD stock_quantity INT
 DEFAULT 0;
+
+-- DML - Data Manipulation Language
+-- insert a new sale in the sales table
+INSERT INTO sales (sale_id, product_id, quantity, sale_date)
+VALUES (106, 3, 1, '2025-09-25');
+
+-- update product stock
+UPDATE products
+SET stock_quantity = stock_quantity - 1
+WHERE product_id = 3;
+
+-- delete a sale from sales table
+DELETE FROM sales
+WHERE sale_id = 106;
+
+-- SELECT *
+-- FROM sales;
+
+-- SELECT *
+-- FROM products;
+
+-- DCL - Data Control Language
+-- 
+
+
+
+
+
+
